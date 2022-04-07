e83888213f4b56bdabad183702a9b944bb1e9796
885685845 <96099327+885685845@users.noreply.github.com>
2022-04-08 02:05:17 +0530

    add

--- src/componants/RestaurantDetails.jsx
+++ src/componants/RestaurantDetails.jsx
@@ -36,7 +36,7 @@ export const RestaurantDetails=()=>{
     return(
         <div>
            
-            <h1 className="nameh1">Restaurants List</h1>
+            <h1 className="nameh1">Restaurant Names</h1>
             <h3>Pagination</h3>
             <div className="pagination">
             <button disabled={page==1} onClick={()=>{setpage((page)=>page - 1)}}>PREV</button>
@@ -50,7 +50,7 @@ export const RestaurantDetails=()=>{
             <button onClick={handlesortacs}>acs</button>
             <button onClick={handlesotdesc}>desc</button>
             </div>
-            <h3>Ratings</h3>
+            <h3>Food Ratings</h3>
             <div className="Ratingbuttons">
             <button onClick={()=>{axios.get("http://localhost:3001/get-restaurants?rating_gte=4").then((res)=>{setdata(res.data)})}}>4 and above</button>
             <button onClick={()=>{axios.get("http://localhost:3001/get-restaurants?rating_gte=3").then((res)=>{setdata(res.data)})}}>3 and above</button>
@@ -58,8 +58,8 @@ export const RestaurantDetails=()=>{
             <button onClick={()=>{axios.get("http://localhost:3001/get-restaurants?rating_gte=1").then((res)=>{setdata(res.data)})}}>1 and above</button>
             <button onClick={()=>{getdata()}}>All</button>
             </div>
-            <h3>Payment Option </h3>
-            <div className="Payment">
+            <h3>Payment Options </h3>
+            <div className="Payment Method">
             <button onClick={()=>{axios.get('http://localhost:3001/get-restaurants').then((res)=>{setdata(res.data.filter((e)=>{if (e.payment_methods.cash===true) {return true} else{return false}}))})}}>Cash</button>
             <button onClick={()=>{axios.get('http://localhost:3001/get-restaurants').then((res)=>{setdata(res.data.filter((e)=>{if (e.payment_methods.card===true) {return true} else{return false}}))})}}>Card</button>
             <button onClick={()=>{axios.get('http://localhost:3001/get-restaurants').then((res)=>{setdata(res.data.filter((e)=>{if (e.payment_methods.upi===true) {return true} else{return false}}))})}}>Upi</button>
@@ -74,22 +74,22 @@ export const RestaurantDetails=()=>{
                     </div>
                    
                     <div className="child2">
-                        <h3>{el.name}</h3>
+                        <h2>{el.name}</h2>
                         {el.cuisine.map((el)=><div key={el} className="cuisine" >
                         <div>
                         <span>{el}</span>
                         </div>
                         </div>)}
-                        <h2 className="cost">Cost For Two:-{el.costForTwo}₹</h2>
-                        <h2 className="cost">Min:-₹{el.minOrder}</h2>
-                        <h2 className="cost">Delivery Time:-{el.deliveryTime}Min</h2>
-                        <div><h2 className="cost">accepts: 
+                        <h3 className="cost">Cost For Two:-{el.costForTwo}₹</h3>
+                        <h3 className="cost">Min:-₹{el.minOrder}</h3>
+                        <h3 className="cost">Delivery Time:-{el.deliveryTime}Min</h3>
+                        <div><h3 className="cost">accepts: 
                         {el.payment_methods.card?' card':''}
                          {el.payment_methods.cash?' cash':''}
                          {el.payment_methods.upi?' upi':''}
                          {el.payment_methods.all?' all':''}
-                         </h2>
-                         <div className="rat">{el.rating}</div>
+                         </h3>
+                         <div className="rat">Rating:-{el.rating}⭐</div>
                         </div>
                          <h3 style={{color:"green"}}>Order Now</h3>
                  </div>
